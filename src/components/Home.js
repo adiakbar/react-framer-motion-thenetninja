a3856df998908011ae924b16f5d88f0c6f775c4c
Adi Akbar <adi.akbar91@gmail.com>
2020-08-16 15:08:54 +0700

    animating routes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,42 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import {motion} from 'framer-motion'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const buttonVariants = {
   hover: {
-    scale: 1.1, // keyframe
+    scale: 1.1,
     textShadow: "0px 0px 8px rgb(255,255,255)",
     boxShadow: "0px 0px 8px rgb(255,255,255)",
     transition: {
       duration: 0.3,
-      yoyo: Infinity // key frame unlimited
+      yoyo: 5
     }
   }
 }
 
+const containerVariants = {
+  hidden: { 
+    opacity: 0, 
+  },
+  visible: { 
+    opacity: 1, 
+    transition: { delay: 1.5, duration: 1.5 }
+  },
+  exit: {
+    x: "-100vh",
+    transition: { ease: 'easeInOut' }
+  }
+};
+
 const Home = () => {
   return (
-    <div className="home container"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 1.5 }}
+    <motion.div className="home container"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
     >
-      <motion.h2 animate={{ fontSize: 50 }}>
-        Welcome to Pizza Joint
-      </motion.h2>
+      <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
         <motion.button
           variants={buttonVariants}
@@ -32,8 +45,8 @@ const Home = () => {
           Create Your Pizza
         </motion.button>
       </Link>
-    </div>
+    </motion.div>
   )
 }
 
-export default Home
+export default Home;
